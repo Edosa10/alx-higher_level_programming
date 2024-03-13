4f37ab389ef474b109bffb70a2ce6960cc024ad9
Ehiorobo Christopher Edosa <ehiorobo.chris@gmail.com>
2024-03-13 13:27:25 -0700

    add

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-number = 98, "Battery street\n"
-print(f"number")
+number = 98, "Battery street"
+print(f"{number}, Battery street,\n")
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
