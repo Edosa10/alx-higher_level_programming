c18d88e66a0c904501d1ff5242364bad276034a2
Ehiorobo Christopher Edosa <ehiorobo.chris@gmail.com>
2024-03-16 14:56:28 -0700

    add

---
+++ python-practice-code/variable.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+hello_int =21
+hello_str ="Hello World"
+hello_bool = False
+hello_tuple = (21,32)
+hello_list =["hello","this","is", "a","list"]
+print(hello_int)
+print(hello_str)
+print(hello_bool)
+print(hello_tuple)
+print(hello_list)
