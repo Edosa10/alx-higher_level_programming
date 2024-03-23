ab4e73e41dec89194793888e376325a4b30c63d3
Ehiorobo Christopher Edosa <ehiorobo.chris@gmail.com>
2024-03-22 20:10:41 -0700

    add

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * dlistint_len - returns the number of elements in
  * a double linked list
@@ -10,20 +9,15 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-
 	count = 0;
-
 	if (h == NULL)
 		return (count);
-
 	while (h->prev != NULL)
 		h = h->prev;
-
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
